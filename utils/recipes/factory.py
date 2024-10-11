01c3b9896b82841e367e22be98d503e0b3f9f22f
Maycon Douglas <dev.maycondouglas@gmail.com>
2024-10-10 22:54:21 -0300

    refac:realização de alguns ajustes que tinham sido esquecido

--- utils/recipes/factory.py
+++ utils/recipes/factory.py
@@ -3,14 +3,10 @@ from random import randint
 
 from faker import Faker
 
-
 def rand_ratio():
     return randint(840, 900), randint(473, 573)
 
-
 fake = Faker('pt_BR')
-# print(signature(fake.random_number))
-
 
 def make_recipe():
     return {
